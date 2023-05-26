bc39db7374098ea4da0e4cc3ff6464506af24665
EJ <hanej93@naver.com>
2023-05-27 08:15:00 +0900

    인증 저장소 - SecurityContextHolder, SecurityContext

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,14 +1,39 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
 	@GetMapping("/")
-	public String index() {
+	public String index(HttpSession session) {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		SecurityContext context = (SecurityContext) session.getAttribute(
+			HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+		Authentication authentication1 = context.getAuthentication();
+
 		return "home";
+	}
+
+	@GetMapping("/thread")
+	public String thread() {
+
+		new Thread(
+			() -> {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			}
+		).start();
+
+		return "thread";
 	}
 
 	@GetMapping("/loginPage")
@@ -40,5 +65,10 @@ public class SecurityController {
 	// public String login() {
 	// 	return "login";
 	// }
+
+	@PostMapping("/")
+	public String postHome() {
+		return "home";
+	}
 
 }
